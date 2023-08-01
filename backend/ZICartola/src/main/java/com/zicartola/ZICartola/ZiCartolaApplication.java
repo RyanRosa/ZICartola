0835f35b49152933455419cb54fddd2b6572ee9b
Ryan <ryan.rosa.profissional@gmail.com>
2023-08-01 05:36:41 -0300

    iniciado implementação do design utilizando o chatgpt

---
+++ backend/ZICartola/src/main/java/com/zicartola/ZICartola/ZiCartolaApplication.java
@@ -0,0 +1,13 @@
+package com.zicartola.ZICartola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiCartolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiCartolaApplication.class, args);
+	}
+
+}
---
+++ backend/ZICartola/src/test/java/com/zicartola/ZICartola/ZiCartolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zicartola.ZICartola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiCartolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
